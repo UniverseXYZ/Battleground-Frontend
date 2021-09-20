19bf48cb1315fcb1d5e2efbc524104c609e30d30
unknown <h.petrosyan07@gmail.com>
2021-09-20 17:38:40 +0300

    feat: developed Wager ETH section

--- src/containers/landingPage/LandingPage.jsx
+++ src/containers/landingPage/LandingPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import HeroSection from '../../components/landingPage/heroSection/HeroSection';
 import HowToPlay from '../howToPlay/HowToPlay';
+import WagerETH from '../../components/landingPage/wagerETH/WagerETH';
 
 const LandingPage = () => (
   <div>
     <HeroSection />
+    <WagerETH />
     <HowToPlay />
   </div>
 );
