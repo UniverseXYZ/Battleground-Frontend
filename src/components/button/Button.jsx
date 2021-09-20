484a0fde9b9c9c454e9e797bc746802655fb0a9a
VahanMuradyan <10vahan.m16@gmail.com>
2021-09-20 17:53:21 +0400

    feat: Header(incomplete)

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,22 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Button.scss';
+import leftShape from '../../assets/images/secondary-button-left.png';
+import rightShape from '../../assets/images/secondary-button-right.png';
 
-const Button = ({ variant, icon, children, ...restProps }) => (
-  <button className={variant} type="button" {...restProps}>
-    {icon && <img src={icon} alt="icon" style={{ marginRight: children ? '11px' : '0' }} />}
-    {children}
+const Button = ({ variant, icon, text, hideTextOnMobile, ...restProps }) => (
+  <button
+    className={`${variant}${hideTextOnMobile ? ' hide--text--on--mobile' : ''}`}
+    type="button"
+    {...restProps}
+  >
+    {icon && (
+      <img className="icon" style={{ marginRight: text ? '11px' : '0px' }} src={icon} alt="icon" />
+    )}
+    <span>{text}</span>
+    {variant === 'secondary' && (
+      <>
+        <img className="left--shape" src={leftShape} alt="left shape" />
+        <img className="right--shape" src={rightShape} alt="right shape" />
+      </>
+    )}
   </button>
 );
 
 Button.propTypes = {
   variant: PropTypes.string.isRequired,
   icon: PropTypes.string,
-  children: PropTypes.oneOfType([PropTypes.any]).isRequired,
+  text: PropTypes.string.isRequired,
+  hideTextOnMobile: PropTypes.bool,
 };
 
 Button.defaultProps = {
   icon: null,
+  hideTextOnMobile: false,
 };
 
 export default Button;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,22 @@
 import React from 'react';
 import './Header.scss';
+import GoBack from '../goBack/GoBack.jsx';
+import Button from '../button/Button.jsx';
+import appLogo from '../../assets/images/app-logo.png';
+import connectWalletIcon from '../../assets/images/icons/plus.svg';
 
-const Header = () => <header>Header</header>;
+const Header = () => (
+  <header>
+    <div className="back--btn">
+      <GoBack text="Home" />
+    </div>
+    <div className="logo">
+      <img src={appLogo} alt="App logo" />
+    </div>
+    <div className="connect--wallet--btn">
+      <Button variant="secondary" text="Connect wallet" icon={connectWalletIcon} hideTextOnMobile />
+    </div>
+  </header>
+);
 
 export default Header;
