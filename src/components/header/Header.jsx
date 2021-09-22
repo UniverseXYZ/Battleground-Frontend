113455fd855d4b1fbaa0e29882e803ec4604ddc0
armen5 <arm.khachatryan11@gmail.com>
2021-09-22 16:32:40 +0400

    feat: Connect wallet popup

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Header.scss';
+import Popup from 'reactjs-popup';
 import GoBack from '../goBack/GoBack.jsx';
 import Button from '../button/Button.jsx';
 import appLogo from '../../assets/images/app-logo.png';
 import connectWalletIcon from '../../assets/images/icons/plus.svg';
+import SelectBattleWalletPopup from '../popups/SelectBattleWalletPopup';
 
 const Header = () => (
   <header>
@@ -12,7 +14,18 @@ const Header = () => (
       <img src={appLogo} alt="App logo" />
     </div>
     <div className="connect--wallet--btn">
-      <Button variant="secondary" text="Connect wallet" icon={connectWalletIcon} hideTextOnMobile />
+      <Popup
+        trigger={
+          <Button
+            variant="secondary"
+            text="Connect wallet"
+            icon={connectWalletIcon}
+            hideTextOnMobile
+          />
+        }
+      >
+        {(close) => <SelectBattleWalletPopup close={close} />}
+      </Popup>
     </div>
   </header>
 );
