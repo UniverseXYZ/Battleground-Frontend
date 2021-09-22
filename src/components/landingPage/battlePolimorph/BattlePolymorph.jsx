0506b57bb8102ab27d1c449a7dacdac85b7db55e
Hovhannes-S <sargsyanhovhannes213@gmail.com>
2021-09-22 18:36:45 +0400

    fix: BattlePolymorphSection border

--- src/components/landingPage/battlePolimorph/BattlePolymorph.jsx
+++ src/components/landingPage/battlePolimorph/BattlePolymorph.jsx
@@ -5,24 +5,26 @@ import borderEffect from '../../../assets/images/landingPage/cut-border.png';
 
 const BattlePolymorph = () => (
   <div className="battle--your--polymorph">
-    <div className="container battle--your--polymorph--section">
+    <div className="battle--polimorph--borders container">
       <img className="top-left-bg" src={borderEffect} alt="border-effect" />
       <img className="bottom-right-bg" src={borderEffect} alt="border-effect" />
-      <div className="battle--your--polymorph--desc">
-        <h1 className="title">Battle your Polymorph</h1>
-        <p>
-          After your battleground has been selected your Polymorph will be entered into a unique
-          Battle pool, where your rival will be randomly chosen.
+      <div className="container battle--your--polymorph--section">
+        <div className="battle--your--polymorph--desc">
+          <h1 className="title">Battle your Polymorph</h1>
+          <p>
+            After your battleground has been selected your Polymorph will be entered into a unique
+            Battle pool, where your rival will be randomly chosen.
+          </p>
+        </div>
+        <div className="battle--your--polymorph--characters">
+          <img src={battlePolymorphCharacters} alt="characters" />
+        </div>
+        <p className="desc-mobile">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip. Duis aute irure dolor quam diolor lorem sit.
         </p>
       </div>
-      <div className="battle--your--polymorph--characters">
-        <img src={battlePolymorphCharacters} alt="characters" />
-      </div>
-      <p className="desc-mobile">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip. Duis aute irure dolor quam diolor lorem sit.
-      </p>
     </div>
   </div>
 );
