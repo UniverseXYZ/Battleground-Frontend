8d913286bcaa78f8884d2ccc02c7960a3cd69720
VahanMuradyan <10vahan.m16@gmail.com>
2021-09-23 14:25:01 +0400

    fix: hide website content while loading

--- src/App.js
+++ src/App.js
@@ -5,10 +5,18 @@ import AppContext from './ContextAPI';
 import Header from './components/header/Header.jsx';
 import Footer from './components/footer/Footer.jsx';
 import LandingPage from './containers/landingPage/LandingPage.jsx';
+import Loading from './components/loading/Loading';
 
 const App = () => {
   const location = useLocation();
   const [isWalletConnected, setIsWalletConnected] = useState(false);
+  const [showLoading, setShowLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowLoading(false);
+    }, 6000);
+  }, []);
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -21,12 +29,18 @@ const App = () => {
         setIsWalletConnected,
       }}
     >
-      <Header />
-      <Switch>
-        <Route exact path="/" component={() => <LandingPage />} />
-        <Route path="*" component={() => <LandingPage />} />
-      </Switch>
-      <Footer />
+      {showLoading ? (
+        <Loading />
+      ) : (
+        <>
+          <Header />
+          <Switch>
+            <Route exact path="/" component={() => <LandingPage />} />
+            <Route path="*" component={() => <LandingPage />} />
+          </Switch>
+          <Footer />
+        </>
+      )}
     </AppContext.Provider>
   );
 };
--- src/containers/landingPage/LandingPage.jsx
+++ src/containers/landingPage/LandingPage.jsx
@@ -1,30 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import HeroSection from '../../components/landingPage/heroSection/HeroSection';
 import BattlePolymorph from '../../components/landingPage/battlePolymorphSection/BattlePolymorph';
 import HowToPlay from '../../components/landingPage/howToPlaySection/HowToPlay';
 import WagerETH from '../../components/landingPage/wagerETHSection/WagerETH';
 import Battlegrounds from '../../components/landingPage/BattlegroundsSection/Battlegrounds';
-import Loading from '../../components/loading/Loading.jsx';
 
-const LandingPage = () => {
-  const [showLoading, setShowLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setShowLoading(false);
-    }, 6000);
-  }, []);
-
-  return (
-    <div>
-      {showLoading && <Loading />}
-      <HeroSection />
-      <BattlePolymorph />
-      <WagerETH />
-      <HowToPlay />
-      <Battlegrounds />
-    </div>
-  );
-};
-
+const LandingPage = () => (
+  <div>
+    <HeroSection />
+    <BattlePolymorph />
+    <WagerETH />
+    <HowToPlay />
+    <Battlegrounds />
+  </div>
+);
 export default LandingPage;
